802e2d6bf6379ffbb32752faf0ac1d892b5de859
greg9702 <grzegorzb9702@gmail.com>
2019-05-21 17:42:28 +0200

    Implemented square class methods

---
+++ cmake-build-debug/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/src/App.cpp.o"
+  "CMakeFiles/game.dir/src/Board.cpp.o"
+  "CMakeFiles/game.dir/src/Piece.cpp.o"
+  "CMakeFiles/game.dir/src/Square.cpp.o"
+  "game.pdb"
+  "game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/Square.cpp
@@ -0,0 +1,51 @@
+//
+// Created by greg9702 on 5/21/19.
+//
+
+#include <Square.h>
+
+Square::Square(char x_, char y_, Piece* piece_) {
+    /**
+     * Constructor
+     * @param x cordinate of board square
+     * @param y corinate of board square
+     * @param Piece object - if piece stand on square, nullptr otherwise
+     */
+
+    //TODO check crectness of x and y asserts
+
+    if (piece_ != nullptr) {
+        this->occupator = piece_;
+    }
+
+    this->x = x_;
+    this->y = y_;
+
+}
+
+Piece* Square::getOccupator() {
+    /**
+     * Return pointer to occupator Piece object
+     * @return pointer to piece object if piece stand on this square nullptr otherwise
+     */
+
+    if (this->occupator != nullptr) {
+        return this->occupator;
+    }
+    return nullptr;
+}
+
+void Square::setOccupator(Piece* occupator_) {
+    /**
+     * Set occupatro
+     * @param pointer to occupator obj //TODO think about nullptr
+     */
+    this->occupator = occupator_;
+}
+
+Square::~Square() {
+    /**
+     * Destructor
+     */
+}
+
