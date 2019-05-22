9b6c70a286b9ff5e422a4b76ab6c92c8d3590e01
greg9702 <grzegorzb9702@gmail.com>
2019-05-22 16:42:18 +0200

    Knight movement done

--- src/Knight.cpp
+++ src/Knight.cpp
@@ -1,5 +1,6 @@
 #include "Knight.h"
-
+#include "Square.h"
+#include "Board.h"
 
 Knight::Knight(color col_, Board* board_, Square* square_) : Piece(col_, board_, square_){
     this->type = KNIGHT;
@@ -9,10 +10,46 @@ Knight::~Knight() {
 
 }
 
-bool Knight::isPossible(char x_, char y__) {
-    return false;
+bool Knight::isPossible(char x_, char y_) {
+    if (!(isCorrect(x_, y_))) {
+        return false;
+    }
+    std::cout << "Is Correct() passed" << std::endl;
+    if ((this->board->getPieceByCoord(x_, y_) != nullptr) &&  (this->board->getMatrix().at(std::pair<char, char>(x_, y_))->getOccupator()->getColor() == this->col)) {
+        return false;
+    }
+
+    return true;
 }
 
 bool Knight::isCorrect(char x_, char y_) {
+
+    std::pair<char, char> current_pos = this->square->getCoords();
+
+    if (current_pos.first + 2 == x_ && current_pos.second + 1 == y_ ) {
+        return true;
+    }
+    if (current_pos.first - 2 == x_ && current_pos.second + 1 == y_ ) {
+        return true;
+    }
+    if (current_pos.first + 2 == x_ && current_pos.second - 1 == y_ ) {
+        return true;
+    }
+    if (current_pos.first - 2 == x_ && current_pos.second - 1 == y_ ) {
+        return true;
+    }
+    if (current_pos.first + 1 == x_ && current_pos.second + 2 == y_ ) {
+        return true;
+    }
+    if (current_pos.first - 1 == x_ && current_pos.second + 2 == y_ ) {
+        return true;
+    }
+    if (current_pos.first + 1 == x_ && current_pos.second - 2 == y_ ) {
+        return true;
+    }
+    if (current_pos.first - 1 == x_ && current_pos.second - 2 == y_ ) {
+        return true;
+    }
+
     return false;
 }
