cefef91ff3829daf02938b3e9289290e13ac3e72
greg9702 <grzegorzb9702@gmail.com>
2019-05-26 19:40:39 +0200

    Fixed cmake.txt and added build and run script

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+project_path=$(pwd)
+# echo $project_path
+# build cpp app
+echo "Building app..."
+if [ -d "build" ]; then
+	echo "Build directory exists"
+else
+	mkdir build
+fi
+cd build
+cmake ..
+make
+
+#build flask app
+cd ..
+cd web
+if [ -d "venv" ]; then
+	echo "Virtual enviroment exists"
+else
+	virtualenv venv
+	source venv/bin/activate
+	pip install -r requirements.txt
+	export FLASK_APP=app.py
+fi
+
+echo "Build sucessfully"
---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source web/venv/bin/activate
+echo "Running application"
+echo "Open your browser 127.0.0.1:5000 to play chess"
+{
+	killall chess
+	./build/chess &
+	cd web
+	flask run
+} &> /dev/null
