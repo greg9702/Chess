7be72db8dcca409f292d2c6a2b400139644bed7b
greg9702 <grzegorzb9702@gmail.com>
2019-05-26 13:59:05 +0200

    playing using mouse

--- web/static/chess.js
+++ web/static/chess.js
@@ -1,1 +1,17 @@
-console.log('XDD')
+let move = ""
+
+function pickSquare(elmnt) {
+	console.log(elmnt.id);
+	move = move + elmnt.id;
+	if (move.length == 4) {
+		sendMove(move);
+	}
+}
+
+function sendMove(move_) {
+	// console.log(move_);
+	move = "";
+	console.log(move);
+	document.body.innerHTML += '<form id="send_move" action="" method="post"><input type="hidden" name="move" value="' + move_ + '"></form>';
+	document.getElementById("send_move").submit();
+}
