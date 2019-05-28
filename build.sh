b2e2b4094aad8c61b115fa36fe4d0d89c98adb39
greg9702 <grzegorzb9702@gmail.com>
2019-05-28 12:24:44 +0200

    Added python3 to venv

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ cd web
 if [ -d "venv" ]; then
 	echo "Virtual enviroment exists"
 else
-	virtualenv venv
+	virtualenv -p python3 venv
 	source venv/bin/activate
 	pip install -r requirements.txt
 	export FLASK_APP=app.py
